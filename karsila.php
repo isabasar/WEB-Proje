c9cf5e5f950551811e0f6b7b9b5bf2f448b97554
isabasar <basar47750@gmail.com>
2025-05-10 22:14:42 +0300

    Login ve php Yapıldı
    Login sayfası Son şekli verildi ve php karşılama sayfası da oluşturulmuştur.

---
+++ karsila.php
@@ -0,0 +1,13 @@
+<?php
+$gercekUname = "G231210059";
+$gerceksifre = "3812572isA";
+$username = $_POST['uname'];
+$password = $_POST['sifre'];
+if($gercekUname == $username && $gerceksifre == $password)
+{echo "Giriş Başarılı".$gercekUname;}
+else{
+    echo "Giriş Başarısız";
+}
+
+
+?>
